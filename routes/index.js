c04de6e8421843036a6af084935f404b673ecf61
김정호 <cloud0406@naver.com>
2021-07-11 17:57:08 +0900

    chore : wired-elements 및 routes설정 변경 [#9]
    * wired-elements 의존성 추가
    * routes 'main.html'에서 'login.html'으로 변경

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.render('main');
+    res.render('login');
 });
 
 module.exports = router;
