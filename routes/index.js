23997b0c6153c2f6d455ed6d0a9b8d8bc690a242
mani.kim <mani.kim@snplab.io>
2021-07-11 12:55:54 +0900

    feat : 로비, 방 생성 페이지 생성 [#12]
    * 페이지 구성
    * 라우팅 적용
    
    Resolves #12

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.render('main');
+    res.render('login');
 });
 
+router.get('/room', async (req, res, next) => {
+    res.render('room');
+});
+
+router.get('/lobby', async (req,res,next) => {
+	res.render('lobby')
+})
 module.exports = router;
