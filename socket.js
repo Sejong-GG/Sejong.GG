1a3c5ab0f041d2289eedfc145553d992587979d5
SeoDaeHyun <sdh4588@gmail.com>
2021-07-12 18:08:41 +0900

    feat : 싱글 룸 생성 [#25]
    - Math.random() 함수를 이용해 난수를 방 아이디로 하는 싱글 방 생성 후 요청한 socket이 바로 입장하도록 설정
    - 추후 문제출제 기능 구현할 때 난수 방 아이디가 문제가 될 시 fix할 예정
    - single의 네임스페이스 이름은 single, url도 /single로 설정함.

--- socket.js
+++ socket.js
@@ -10,4 +10,31 @@ module.exports = (server, app, sessionMiddleware) => {
     //cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
     sessionMiddleware(socket.request, socket.request.res, next);
   });
+
+  single.on('connection', (socket)=>
+  {
+    console.log('싱글서버 접속');
+    const req=socket.request;
+    const singleRoomId=Math.random();
+    //랜덤 난수가 완벽히 똑같기 힘든 점을 이용
+    //굳이 프론트에서 링크로 요청을 받지 않고 난수로 아이디 생성
+    console.log(singleRoomId);
+    socket.join(singleRoomId);
+    var hints=['a','b'];
+    socket.on('get', ()=>
+    {
+      socket.to(singleRoomId).emit('make',{
+        answer : 'ans',
+        hints: hints
+      });
+    })
+    
+
+    socket.on('disconnect',()=>{
+      socket.leave(singleRoomId);
+    });
+    
+
+  })
+
 }
