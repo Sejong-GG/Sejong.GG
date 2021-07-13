b6d4842b7169fbf74e1d1a1b05b7915147c4717c
mani.kim <mani.kim@snplab.io>
2021-07-13 17:42:34 +0900

    hanui : 확인버튼 구현중

--- socket.js
+++ socket.js
@@ -21,7 +21,34 @@ module.exports = (server, app, sessionMiddleware) => {
     //굳이 프론트에서 링크로 요청을 받지 않고 난수로 아이디 생성
     console.log(singleRoomId);
     socket.join(singleRoomId);
+    const champions = ["amumu", "janna", "katarina", "garen", "lux", "gnar", "zed", "annie", "monkeyking", "akali", "camille", "drmundo", "kennen"];
+    //받아올 챔 이름들
 
+    function sendQuizSet()
+    {
+      var quizSet=[];//넘길 퀴즈셋 초기화
+      var randomtestIndex=0;
+
+      Chams.find({name:champions[randomtestIndex]}, function(err,docs)
+      {
+        var randomIndex=[0,1,2,3];
+        for(var i=0;i<4;i++)
+        {
+          quizSet[i]=docs[randomIndex[i]]
+        }
+        return quizSet;
+      });
+    }
+
+    socket.on('make', ()=>//make 요청이 오면,
+    {
+	  console.log('make 실행');
+      var quizSet=[];
+      quizSet=sendQuizSet();
+      socket.to(singleRoomId).emit('get', quizSet);
+      //해당 유저의 singleRoom에 get 으로 퀴즈셋 전송
+    });
+    
     socket.on('disconnect',()=>{
       socket.leave(singleRoomId);
     });
