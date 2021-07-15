559dff46547c09011c7e81d41da5b8204a8db089
riyenas0925 <riyenas0925@gmail.com>
2021-07-15 21:49:37 +0900

    refactor : href 경로 변경, html 변경

--- schemas/chat.js
+++ schemas/chat.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { Types: { ObjectId } } = Schema;
 const chatSchema = new Schema({
-  user: {
-    type: String,
-    required: true,
-  },
-  chat: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+	user: {
+		type: String,
+    	required: true,
+  	},
+  	chat: {
+	    type: String,
+	    required: true,
+  	},
+  	createdAt: {
+	    type: Date,
+    	default: Date.now,
+	},
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
